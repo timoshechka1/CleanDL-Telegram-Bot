91ddcc313b346f4007772b89717ff3e44eaddf0a
timoshechka1 <ttimoshechka@gmail.com>
2025-04-24 13:56:09 +0300

    fix: Rewritten function download_video

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,6 +1,3 @@
-import re
-
-from aiogram import types
 from telegram import Update
 from telegram.ext import ContextTypes
 from bot.cobalt import get_direct_video_url
@@ -10,19 +7,11 @@ URL_REGEX = r'https?://[^\s]+'
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Приветсвую. Я Телеграм Бот CleanDL, отправьте мне ссылку и я загружу вам видео 🎬")
 
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    message_text = update.message.text
-
-    if re.match(URL_REGEX, message_text):
-        await update.message.reply_text("Ссылка принята. Идёт обработка… ⏳")
-    else:
-        await update.message.reply_text("Пожалуйста, отправь ссылку на видео 📎")
-
-async def download_video(message: types.Message):
-    video_link = message.text.strip()
+async def download_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    video_link = update.message.text.strip()
 
     direct_url = get_direct_video_url(video_link)
     if direct_url:
-        await message.reply_video(direct_url)
+        await update.message.reply_video(direct_url)
     else:
-        await message.reply("❌ Не удалось получить видео.")
+        await update.message.reply_text("❌ Не удалось получить видео.")
--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,12 @@
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 from config import BOT_TOKEN
-from bot.handlers import start, handle_message
+from bot.handlers import start, download_video
 
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, download_video))
 
     print("Бот Запущен...")
 
