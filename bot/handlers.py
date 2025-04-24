0bffae17e5349c656201a4a898343d721357f4e4
timoshechka1 <ttimoshechka@gmail.com>
2025-04-24 13:48:16 +0300

    feat: Adding video download function

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,8 +1,9 @@
 import re
 
+from aiogram import types
 from telegram import Update
 from telegram.ext import ContextTypes
-
+from bot.cobalt import get_direct_video_url
 
 URL_REGEX = r'https?://[^\s]+'
 
@@ -16,3 +17,12 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("Ссылка принята. Идёт обработка… ⏳")
     else:
         await update.message.reply_text("Пожалуйста, отправь ссылку на видео 📎")
+
+async def download_video(message: types.Message):
+    video_link = message.text.strip()
+
+    direct_url = get_direct_video_url(video_link)
+    if direct_url:
+        await message.reply_video(direct_url)
+    else:
+        await message.reply("❌ Не удалось получить видео.")
