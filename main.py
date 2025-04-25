f04fb200648669982d997905ce3ad6c96010aa86
timoshechka1 <ttimoshechka@gmail.com>
2025-04-25 21:08:24 +0300

    fix: Rewritten main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from aiogram import Router
-from aiogram.types import Message
-from aiogram.filters import Command
+from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
+from aiogram.fsm.storage.memory import MemoryStorage
+import asyncio
 
-router = Router()
+from config import BOT_TOKEN
+from bot.handlers import router
 
-@router.message(Command("start"))
-async def start_handler(message: Message):
-    await message.answer("Привет, я бот!")
+async def main():
+    bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp = Dispatcher(storage=MemoryStorage())
 
-@router.message()
-async def handle_tiktok_link(message: Message):
-    await message.answer("Ты отправил ссылку!")
-    
+    dp.include_router(router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
